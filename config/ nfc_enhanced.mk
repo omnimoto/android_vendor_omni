22de3b766d8ba996e27bb0f6af4144da84de70d4
Validus13 <groundzeroroms@gmail.com>
2013-12-30 12:53:29 -0500

    Update  nfc_enhanced.mk

--- config/ nfc_enhanced.mk
+++ config/ nfc_enhanced.mk
@@ -1,3 +1,3 @@
 # This device supports CM enhanced NFC
 PRODUCT_COPY_FILES += \
-    vendor/omni/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
+    vendor/omni/config/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
